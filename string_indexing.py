92822109e57013b1e17c336e41bccf974193cd5d
MaksimSmykov <smykovmaksim23@yandex.ru>
2024-08-24 10:24:50 +0300

    Add files via upload

---
+++ string_indexing.py
@@ -0,0 +1,13 @@
+example = 'Топинамбур'
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1:10:2])
+print('\n')
+b = 'Телефон'
+print(b[0])
+print(b[-1])
+print(b[4:])
+print(b[::-1])
+print(b[1:6:2])
