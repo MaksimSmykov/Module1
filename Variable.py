3a6908fcdc7466787ab44cb2add26556ef0196fc
MaksimSmykov <smykovmaksim23@yandex.ru>
2024-08-25 08:51:21 +0300

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,9 @@
+count_of_homeworks = 12
+count_of_spent_hours = 1.5
+course_name = 'Python'
+time_for_one_task = count_of_spent_hours/count_of_homeworks
+print('Курс:', course_name, ', всего задач:', count_of_homeworks, ', затрачено часов:',count_of_spent_hours,', среднее время выполнения',
+      time_for_one_task,'часа.')
+print('или')
+print('Курс: ' + str(course_name) + ', всего задач:' + str(count_of_homeworks) +
+      ', затрачено часов: ' + str(count_of_spent_hours) + ', среднее время выполнения ' + str(time_for_one_task) + ' часа.')
