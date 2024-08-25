8a6c7fd6aa3ff7b079d4c41104943f88cf290907
MaksimSmykov <smykovmaksim23@yandex.ru>
2024-08-25 08:53:03 +0300

    Variable.py

--- Variable.py
+++ Variable.py
@@ -5,5 +5,5 @@ time_for_one_task = count_of_spent_hours/count_of_homeworks
 print('Курс:', course_name, ', всего задач:', count_of_homeworks, ', затрачено часов:',count_of_spent_hours,', среднее время выполнения',
       time_for_one_task,'часа.')
 print('или')
-print('Курс: ' + str(course_name) + ', всего задач:' + str(count_of_homeworks) +
+print('Курс: ' + str(course_name) + ', всего задач: ' + str(count_of_homeworks) +
       ', затрачено часов: ' + str(count_of_spent_hours) + ', среднее время выполнения ' + str(time_for_one_task) + ' часа.')
